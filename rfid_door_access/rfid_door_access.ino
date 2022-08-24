9751e6dc25a42d01249a18a4924eb24568d86f59
Isaac J.Newton <isaacjavannewton@gmail.com>
2022-08-24 14:30:12 +0100

    made some changes

--- rfid_door_access/rfid_door_access.ino
+++ rfid_door_access/rfid_door_access.ino
@@ -2,13 +2,19 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal.h>
+#include <Servo.h>
+Servo servo;
 
 #define redLed A4
 #define greenLed A5
 #define buzzer A3
+#define servoPin 11
+
+
 bool programMode = false;
-String uid;
-int lastPos;
+
+#define openPos 0
+#define closePos 90
 
 uint8_t successRead;    // Variable integer to keep if we have Successful Read from Reader
 byte storedCard[4];   // Stores an ID read from EEPROM
@@ -16,9 +22,9 @@ byte readCard[4];   // Stores scanned ID read from RFID Module
 byte masterCard[4];   // Stores master card's ID read from EEPROM
 #define SS_PIN 10
 #define RST_PIN 9
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-String cards[10] = {}; // array that stores uid cards
 
 
 void setup() {
@@ -36,6 +42,8 @@ void setup() {
   }
   digitalWrite(buzzer, 0);
 
+  servo.attach(servoPin);
+  
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
@@ -48,17 +56,6 @@ void setup() {
     Serial.println(F("Scan A PICC to Define as Master Card"));
     do {
       successRead = getID();  
-      for(int y = 0; y < 2; ++y)
-      {
-        lcd.setCursor(14 + y, 1);
-        lcd.print('.');
-        delay(250);      
-      }
-      for(int y = 0; y < 2; ++y)
-      {
-        lcd.setCursor(14 + y, 1);
-        lcd.print(' ');    
-      }
     }while (!successRead);
     for ( uint8_t j = 0; j < 4; j++ ) {       
       EEPROM.write( 2 + j, readCard[j] );
@@ -127,72 +124,221 @@ void loop() {
     }
     else {
       if (findID(readCard)) {
-        int index = lookUp(cards, sizeof(cards), compileId(readCard, sizeof(readCard)));
-        Serial.println("Index is " + String(index));
-        if(!index)
-        {
-          int pos = lastPos;
-          cards[pos] = compileId(readCard, sizeof(readCard));
-          status[pos] = "signedIn";
-          Serial.print(F("Welcome! "));
-          Serial.println(compileId(readCard, sizeof(readCard)));
-          granted(300);
-          lastPos++;
-          delay(2000);
-        }
-        else if(index > 0 && status[index - 1] == "signedIn") {
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("#ACCESS GRANTED#");
-          lcd.setCursor(0, 1);
-          delay(500);
-          lcd.print("ID: " + compileId(readCard, sizeof(readCard)));
-          delay(500);
-          lcd.setCursor(0, 1);
-          lcd.print("Bye " + users[index-1]);
-          granted(300);
-          status[index - 1] = "signedOut";
-          delay(2000);
-          lcd.setCursor(0, 0);
-          lcd.print("Place a Card:   ");
-          lcd.setCursor(0, 1);
-          lcd.print("                ");
-        }
-        else if(index > 0 && status[index - 1] == "signedOut")
-        {
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("#ACCESS GRANTED#");
-          lcd.setCursor(0, 1);
-          delay(500);
-          lcd.print("ID: " + compileId(readCard, sizeof(readCard)));
-          delay(500);
-          lcd.setCursor(0, 1);
-          lcd.print("Welcome" + users[index-1]);
-          granted(300);
-          status[index - 1] = "signedIn";
-          delay(2000);
-          lcd.setCursor(0, 0);
-          lcd.print("Place a Card:   ");
-          lcd.setCursor(0, 1);
-          lcd.print("                ");
-        }
+        //granted
+        granted();
+        openDoor(10);
       }
       else {
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("#ACCESS DENIED#");
-        lcd.setCursor(0, 1);
-        delay(500);
-        lcd.print("I don't know you");
-        Serial.println(F("I don't know you"));
+        //denied
         denied();
-        delay(2000);
-        lcd.setCursor(0, 0);
-        lcd.print("Place a Card:   ");
-        lcd.setCursor(0, 1);
-        lcd.print("                ");
       }
     }
   }
+}
+
+void granted() {
+  for(int x = 0; x < 3; ++x)
+  {
+    digitalWrite(buzzer, 1);
+    delay(50);
+    digitalWrite(buzzer, 0);
+    delay(50);
+  }
+  digitalWrite(redLed, 0);
+  digitalWrite(greenLed, 1);
+}
+void denied() {
+  for(int x = 0; x < 2; ++x)
+  {
+    digitalWrite(buzzer, 1);
+    delay(200);
+    digitalWrite(buzzer, 0);
+    delay(100);
+  }
+  digitalWrite(greenLed, 0);  // Make sure green LED is off
+  digitalWrite(redLed, 1);   // Turn on red LED
+}
+uint8_t getID() {
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return 0;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return 0;
+  }
+  Serial.println(F("Scanned PICC's UID:"));
+  for ( uint8_t i = 0; i < 4; i++) {  
+    readCard[i] = mfrc522.uid.uidByte[i];
+    Serial.print(readCard[i], HEX);
+  }
+  Serial.println("");
+  mfrc522.PICC_HaltA();
+  return 1;
+}
+
+void ShowReaderDetails() {
+  byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
+  Serial.print(F("MFRC522 Software Version: 0x"));
+  Serial.print(v, HEX);
+  if (v == 0x91)
+    Serial.print(F(" = v1.0"));
+  else if (v == 0x92)
+    Serial.print(F(" = v2.0"));
+  else
+    Serial.print(F(" (unknown),probably a chinese clone?"));
+  Serial.println("");
+  if ((v == 0x00) || (v == 0xFF)) {
+    Serial.println(F("WARNING: Communication failure, is the MFRC522 properly connected?"));
+    Serial.println(F("SYSTEM HALTED: Check connections."));
+    // Visualize system is halted
+    digitalWrite(greenLed, 0);  // Make sure green LED is off
+    digitalWrite(redLed, 1);   // Turn on red LED
+    while (true); // halt
+  }
+}
+void cycleLeds() {
+  digitalWrite(redLed, 0);  
+  digitalWrite(greenLed, 1);  
+  delay(200);
+  digitalWrite(redLed, 1);  
+  digitalWrite(greenLed, 0);    
+}
+void normalModeOn () {
+  digitalWrite(redLed, 1);
+  digitalWrite(greenLed, 0);
+}
+
+void readID( uint8_t number ) {
+  uint8_t start = (number * 4 ) + 2;    // Figure out starting position
+  for ( uint8_t i = 0; i < 4; i++ ) {     // Loop 4 times to get the 4 Bytes
+    storedCard[i] = EEPROM.read(start + i);   // Assign values read from EEPROM to array
+  }
+}
+void writeID( byte a[] ) {
+  if ( !findID( a ) ) {
+    uint8_t num = EEPROM.read(0);    
+    uint8_t start = ( num * 4 ) + 6;
+    num++;               
+    EEPROM.write( 0, num );
+    for ( uint8_t j = 0; j < 4; j++ ) {
+      EEPROM.write( start + j, a[j] );
+    }
+    successWrite();
+    Serial.println(F("Succesfully added ID record to memory"));
+  }
+  else {
+    failedWrite();
+    Serial.println(F("Failed! There is something wrong with ID or memory"));
+  }
+}
+void deleteID( byte a[] ) {
+  if ( !findID( a ) ) {
+    failedWrite();     
+    Serial.println(F("Failed! There is something wrong with ID or bad EEPROM"));
+  }
+  else {
+    uint8_t num = EEPROM.read(0);  
+    uint8_t slot;      
+    uint8_t start;     
+    uint8_t looping;    
+    uint8_t j;
+    uint8_t count = EEPROM.read(0);
+    slot = findIDSLOT( a );
+    start = (slot * 4) + 2;
+    looping = ((num - slot) * 4);
+    num--;      
+    EEPROM.write( 0, num );  
+    for ( j = 0; j < looping; j++ ) {        
+      EEPROM.write( start + j, EEPROM.read(start + 4 + j));
+    }
+    for ( uint8_t k = 0; k < 4; k++ ) {
+      EEPROM.write( start + j + k, 0);
+    }
+    successDelete();
+    Serial.println(F("Succesfully removed ID record from memory"));
+  }
+}
+bool checkTwo ( byte a[], byte b[] ) {   
+  for ( uint8_t k = 0; k < 4; k++ ) {
+    if ( a[k] != b[k] ) {
+       return false;
+    }
+  }
+  return true;  
+}
+uint8_t findIDSLOT( byte find[] ) {
+  uint8_t count = EEPROM.read(0);   
+  for ( uint8_t i = 1; i <= count; i++ ) { 
+    readID(i);
+    if ( checkTwo( find, storedCard ) ) { 
+      return i;
+    }
+  }
+}
+bool findID( byte find[] ) {
+  uint8_t count = EEPROM.read(0);
+  for ( uint8_t i = 1; i < count; i++ ) {
+    readID(i);      
+    if ( checkTwo( find, storedCard ) ) { 
+      return true;
+    }
+    else { 
+    }
+  }
+  return false;
+}
+void successWrite() {
+  digitalWrite(redLed, 0); 
+  digitalWrite(greenLed, 1); 
+  for(int x = 0; x < 3; ++x)
+  {
+    digitalWrite(buzzer, 1);
+    delay(50);
+    digitalWrite(buzzer, 0);
+    delay(50);
+  }
+  delay(1000);
+  digitalWrite(redLed, 0); 
+  digitalWrite(greenLed, 0);
+}
+void failedWrite() {
+  digitalWrite(redLed, 1); 
+  digitalWrite(greenLed, 0); 
+  for(int x = 0; x < 2; ++x)
+  {
+    digitalWrite(buzzer, 1);
+    delay(200);
+    digitalWrite(buzzer, 0);
+    delay(100);
+  }
+  for(int x = 0; x < 2; ++x)
+  {
+    digitalWrite(redLed, 1);
+    delay(100);
+    digitalWrite(redLed, 0);
+    delay(100);
+  }
+}
+void successDelete() {
+  digitalWrite(redLed, 0); 
+  digitalWrite(greenLed, 1); 
+  for(int x = 0; x < 3; ++x)
+  {
+    digitalWrite(buzzer, 1);
+    delay(50);
+    digitalWrite(buzzer, 0);
+    delay(50);
+  }
+  delay(1000);
+  digitalWrite(redLed, 0); 
+  digitalWrite(greenLed, 0);
+}
+bool isMaster( byte test[] ) {
+  return checkTwo(test, masterCard);
+}
+
+void openDoor(float Delay)
+{
+  servo.write(openPos);
+  delay(int(Delay * 1000));
+  servo.write(closePos);
 }
